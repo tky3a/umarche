e9d1bedfd788ebc48141eba838bf68d9ce9f8287
tky <takayamac@icloud.com>
2022-08-16 10:50:41 +0900

    コンポーネントに変数で値を渡す方法のサンプル

--- app/Http/Controllers/ComponentTextController.php
+++ app/Http/Controllers/ComponentTextController.php
@@ -8,7 +8,8 @@ class ComponentTextController extends Controller
 {
     public function showComponent1()
     {
-        return view('tests.component-test1');
+        $message = "メッセージを送信";
+        return view('tests.component-test1', compact('message'));
     }
 
     public function showComponent2()
--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -5,5 +5,5 @@
   コンポーネント１
 
   {{-- propsを使う例 --}}
-  <x-tests.card title="タイトル" content="本文" />
+  <x-tests.card title="タイトル" content="本文" :message="$message" />
 </x-tests.app>
