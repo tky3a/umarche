7714c0de15cb0f4e26d962a93f3dd8b7e96f8709
tky <takayamac@icloud.com>
2022-08-16 19:47:15 +0900

    マルチログインのmiddleware設定。ログイン済みユーザーがアクセスしたらリダイレクト処理を追加

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -9,6 +9,10 @@ use Illuminate\Support\Facades\Auth;
 
 class RedirectIfAuthenticated
 {
+    // 以下定数はconfig/auth.phpで設定した「guards」の値と同じものを設定する
+    private const GUARD_USER = 'users';
+    private const GUARD_OWNER = 'owners';
+    private const GUARD_ADMIN = 'admin';
     /**
      * Handle an incoming request.
      *
@@ -16,15 +20,23 @@ class RedirectIfAuthenticated
      * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
      * @param  string|null  ...$guards
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     * 認証済みユーザーがアクセスしたら特定の画面にリダイレクトする
      */
     public function handle(Request $request, Closure $next, ...$guards)
     {
-        $guards = empty($guards) ? [null] : $guards;
+        // Userが認証済み && ルートがuserの場合
+        if (Auth::guard(self::GUARD_USER)->check() && $request->routeIs('user.*')) {
+            return redirect(RouteServiceProvider::HOME);
+        }
 
-        foreach ($guards as $guard) {
-            if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
-            }
+        // Ownerが認証済み && ルートがownerの場合
+        if (Auth::guard(self::GUARD_OWNER)->check() && $request->routeIs('owner.*')) {
+            return redirect(RouteServiceProvider::OWNER_HOME);
+        }
+
+        // Adminが認証済み && ルートがadminの場合
+        if (Auth::guard(self::GUARD_ADMIN)->check() && $request->routeIs('admin.*')) {
+            return redirect(RouteServiceProvider::ADMIN_HOME);
         }
 
         return $next($request);
