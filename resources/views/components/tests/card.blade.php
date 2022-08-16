d1223bdc6bb62c0b03f817b3d0aca0370e8fcb49
tky <takayamac@icloud.com>
2022-08-16 10:32:58 +0900

    コンポーネントでpropsで値をうけとるサンプル

---
+++ resources/views/components/tests/card.blade.php
@@ -0,0 +1,5 @@
+<div class="w-1/4 border-2 p-2 shadow-md">
+  <div>{{ $title }}</div>
+  <div>画像</div>
+  <div>{{ $content }}</div>
+</div>
--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -3,4 +3,7 @@
     へっだー１
   </x-slot>
   コンポーネント１
+
+  {{-- propsを使う例 --}}
+  <x-tests.card title="タイトル" content="本文" />
 </x-tests.app>
